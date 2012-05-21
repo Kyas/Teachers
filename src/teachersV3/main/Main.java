6e212d2fdda06f5fab00ab220c0ada0c7d799352
Jeremy Lor <Jrmy Lor@Jeremy-PC>
2012-05-22 01:40:57 +0200

    Update of V3 : jFreeChart

--- src/teachersV3/main/Main.java
+++ src/teachersV3/main/Main.java
@@ -1,42 +1,13 @@
 package teachersV3.main;
 
-import java.io.IOException;
-import java.util.NoSuchElementException;
-
-import teachersV2.io.FileRead;
-import teachersV2.io.FileReadService;
-import teachersV3.jfreechart.Pie;
+import teachersV3.jFreeChart.PieChart;
 
 public class Main {
 
 	public static void main(String[] args) {
-
-		try {
-			// Command Line with the name of the file as an argument.
-			FileReadService fr = new FileRead();
-			for (int i = 0; i < args.length; i++) {
-				System.out.println("Reading: " + args[i]);
-				String path = args[i];
-				fr.read(path, ";");
-			}
-		} catch (IOException e) {
-			System.err
-					.println("File Not Found !\nAre you sure this is the right file/place ?\n");
-		} catch (IndexOutOfBoundsException e) {
-			System.err.println("Error Columns Found on the file !\nReason: "
-					+ e.getLocalizedMessage());
-		} catch (NumberFormatException e) {
-			System.err
-					.println("/!\\ Please, format well your file .csv !\nReason : "
-							+ e.getLocalizedMessage() + "\n");
-		} catch (NoSuchElementException e) {
-			System.err.println("No response found !");
-		}
-
-		Pie demo = new Pie("Student Marks", "Jeremy Lor", 0);
-		demo.pack();
-		demo.setVisible(true);
-		System.exit(0);
-	}
+        PieChart demo = new PieChart("Comparison", "Which operating system are you using?");
+        demo.pack();
+        demo.setVisible(true);
+    }
 
 }
